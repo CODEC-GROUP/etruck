0b7bfa4af00e697a575956b8e5fac00b092d42fe
Christian Nana <87042400+Txchrixo@users.noreply.github.com>
2023-06-09 18:08:14 +0100

    plasticModel

--- Model/PlasticModel.php
+++ Model/PlasticModel.php
@@ -12,9 +12,90 @@ class PlasticModel
         $this->DB = $BD;
     }
 
+    public function insertPlastic($categoryId, $poster, $posterId, $image, $will, $location, $status, $crt_date, $upd_date)
+    {
+        $data = [
+            'category_id' => $categoryId,
+            'poster' => $poster,
+            'poster_id' => $posterId,
+            'image' => $image,
+            'will' => $will,
+            'location' => $location,
+            'status' => $status,
+            'crt_date' => $crt_date,
+            'upd_date' => $upd_date,
+        ];
 
+        $stmt = $this->DB->query("INSERT INTO plastic (category_id, poster, poster_id, image, will, location, status, crt_date, upd_date) VALUES (:category_id, :poster, :poster_id, :image, :will, :location, :status, :crt_date, :upd_date)", $data, 'insert');
 
+        return $stmt;
+    }
 
+    public function reservePlastic($plasticId, $truckId)
+    {
+        $data = [
+            'plastic_id' => $plasticId,
+            'truck_id' => $truckId,
+        ];
+
+        $stmt = $this->DB->query("UPDATE plastic SET truck_id=:truck_id WHERE id=:plastic_id", $data, 'update');
+
+        return $stmt;
+    }
+
+    public function updatePlastic($plasticId, $categoryId, $poster, $posterId, $image, $will, $location, $status, $upd_date)
+    {
+        $data = [
+            'plastic_id' => $plasticId,
+            'category_id' => $categoryId,
+            'poster' => $poster,
+            'poster_id' => $posterId,
+            'image' => $image,
+            'will' => $will,
+            'location' => $location,
+            'status' => $status,
+            'upd_date' => $upd_date,
+        ];
+
+        $stmt = $this->DB->query("UPDATE plastic SET category_id=:category_id, poster=:poster, poster_id=:poster_id, image=:image, will=:will, location=:location, status=:status, upd_date=:upd_date WHERE id=:plastic_id", $data, 'update');
+
+        return $stmt;
+    }
+
+    public function getPlastics()
+    {
+        $rows = $this->DB->query("SELECT * FROM plastic");
+        if ($rows) {
+            $output = '';
+            // Creating table of data for plastics
+            foreach ($rows as $key => $row) {
+                $output .=
+                    '<tr>
+                    <td>' . ($key + 1) . '</td>
+                    <td>' . $row->category_id . '</td>
+                    <td>' . $row->poster . '</td>
+                    <td>' . $row->poster_id . '</td>
+                    <td>' . $row->truck_id . '</td>
+                    <td>' . $row->image . '</td>
+                    <td>' . $row->will . '</td>
+                    <td>' . $row->location . '</td>
+                    <td>' . $row->status . '</td>
+                    <td>' . $row->crt_date . '</td>
+                    <td>' . $row->upd_date . '</td>
+                </tr>';
+            }
+            return $output;
+        } else {
+            return "empty Table";
+        }
+    }
+
+    public function getPlasticsByType($categoryId)
+    {
+        $result = $this->DB->query("SELECT * FROM plastic WHERE category_id = ?", [$categoryId]);
+        return $result;
+    }
 }
+
 
 ?>
--- etruck.sql
+++ etruck.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 09, 2023 at 04:05 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 8.0.19
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 09, 2023 at 05:04 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,13 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `crt_date` text NOT NULL,
+  `upd_date` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE IF NOT EXISTS `company` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `phone_number` text NOT NULL,
+  `password` text NOT NULL,
+  `city` text NOT NULL,
+  `qwater` text NOT NULL,
+  `frequency` text NOT NULL,
+  `crt_date` text NOT NULL,
+  `upd_date` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `plastic`
 --
 
-CREATE TABLE `plastic` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `plastic`;
+CREATE TABLE IF NOT EXISTS `plastic` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `category_id` varchar(255) NOT NULL,
-  `poster` int(11) NOT NULL,
+  `poster` int NOT NULL,
   `poster_id` varchar(255) NOT NULL,
   `truck_id` varchar(255) NOT NULL,
   `image` text NOT NULL,
@@ -38,28 +74,57 @@ CREATE TABLE `plastic` (
   `loaction` text NOT NULL,
   `status` text NOT NULL,
   `crt_date` text NOT NULL,
+  `upd_date` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `truckman`
+--
+
+DROP TABLE IF EXISTS `truckman`;
+CREATE TABLE IF NOT EXISTS `truckman` (
+  `id` int NOT NULL,
+  `name` text NOT NULL,
+  `phone_number` text NOT NULL,
+  `password` text NOT NULL,
+  `city` text NOT NULL,
+  `qwater` text NOT NULL,
+  `gender` text NOT NULL,
+  `frequency` text NOT NULL,
+  `crt_date` text NOT NULL,
   `upd_date` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `user`
 --
 
---
--- Indexes for table `plastic`
---
-ALTER TABLE `plastic`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `phone_number` text NOT NULL,
+  `password` text NOT NULL,
+  `city` text NOT NULL,
+  `qwater` text NOT NULL,
+  `gender` text NOT NULL,
+  `frequency` text NOT NULL,
+  `crt_date` text NOT NULL,
+  `upd_date` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `user`
 --
 
---
--- AUTO_INCREMENT for table `plastic`
---
-ALTER TABLE `plastic`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `user` (`id`, `name`, `phone_number`, `password`, `city`, `qwater`, `gender`, `frequency`, `crt_date`, `upd_date`) VALUES
+(1, 'eta cyril', '651898704', '1234', 'Douala', 'pk17', 'male', '2\r\n', '', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
